ed8306dbc46450bddbe8c719c7ed2c424b651005
DiyaFarza <diyaffarza@gmail.com>
2026-02-26 09:11:32 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,22 @@
-public class PalindromeCheckerApp {
-    public static  void main(String[] args)
-    {
-        System.out.println("welcome to palindrome checker app");
-        System.out.println("use case 1");
-    }
 
-}
+public class PalindromeCheckerApp {
+    public static  void main(String[] args) {
+
+                String str = "madam";
+                String reversed = "";
+
+                // Reverse the string
+                for (int i = str.length() - 1; i >= 0; i--) {
+                    reversed += str.charAt(i);
+                }
+
+                // Check if original and reversed strings are equal
+                if (str.equals(reversed)) {
+                    System.out.println(str + " is a palindrome.");
+                } else {
+                    System.out.println(str + " is not a palindrome.");
+                }
+            }
+        }
+
+
